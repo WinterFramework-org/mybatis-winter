906bc2dda9b7bb6870d77503164479119761dbc6
신성현 <froggy0m0a@gmail.com>
2025-06-23 00:16:59 +0900

    feat: DataSource 구성 방식 변경
    - 기존 'XML' → 'properties' 기반으로 전환
    - WinterFramework 프로퍼티 기능을 이용해 URL, username, password 주입
    
    ref: WinterFramework-org/winter-framework@a13a1129db17f972e5c27be66f4e5b93ab2141d6

--- src/main/java/froggy/mybatis/winter/SqlSessionFactory.java
+++ src/main/java/froggy/mybatis/winter/SqlSessionFactory.java
@@ -1,9 +1,12 @@
 package froggy.mybatis.winter;
 
+import froggy.winterframework.beans.factory.annotation.Autowired;
+import froggy.winterframework.beans.factory.annotation.Value;
 import froggy.winterframework.stereotype.Component;
 import java.io.IOException;
 import java.io.InputStream;
 import org.apache.ibatis.builder.xml.XMLConfigBuilder;
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
@@ -11,22 +14,29 @@ import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
 @Component
 public class SqlSessionFactory extends DefaultSqlSessionFactory {
 
-    public SqlSessionFactory() {
-        this(loadConfiguration());
+    private static final String MYBATIS_CONFIG = "mybatis-config.xml";
+
+    @Autowired
+    public SqlSessionFactory(
+        @Value("winter.datasource.url") String url,
+        @Value("winter.datasource.username") String username,
+        @Value("winter.datasource.password") String password
+    ) {
+        super(load(url, username, password));
     }
 
-    public SqlSessionFactory(Configuration configuration) {
-        super(configuration);
-    }
+    private static Configuration load(String url, String username, String password) {
+        try (InputStream in = Resources.getResourceAsStream(MYBATIS_CONFIG)) {
+            Configuration cfg = new XMLConfigBuilder(in).parse();
 
-    private static Configuration loadConfiguration() {
-        String resource = "mybatis-config.xml";
-        try (InputStream inputStream = Resources.getResourceAsStream(resource)) {
-            XMLConfigBuilder parser = new XMLConfigBuilder(inputStream);
-            return parser.parse();
+            PooledDataSource ds = (PooledDataSource) cfg.getEnvironment().getDataSource();
+            ds.setUrl(url);
+            ds.setUsername(username);
+            ds.setPassword(password);
+
+            return cfg;
         } catch (IOException e) {
-            throw new RuntimeException("Failed to load MyBatis configuration: " + resource, e);
+            throw new RuntimeException(e);
         }
     }
-
 }
